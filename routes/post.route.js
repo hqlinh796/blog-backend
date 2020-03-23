eb03cecf0473ccb7aff6490aee76bde017f07a70
linh <linhknife796@gmail.com>
2020-03-23 11:17:50 +0700

    add infiniti scroll info into list posts router

--- routes/post.route.js
+++ routes/post.route.js
@@ -6,15 +6,24 @@ const postModel = require('../models/post.model');
 
 //get all post
 router.get('/', async (req, res, next) => {
-    const page = req.query.page || 0,
-          limit = req.query.limit || 5;
+    const page = parseInt(req.query.page || 0),
+          limit = parseInt(req.query.limit || 2);
 
     try {
-        const postArray = await postModel.find().skip(page*limit).limit(limit).populate('comments', '_id');
-        //console.log(postArray);
+        const  [totalPosts, posts] = await Promise.all(  
+            [   postModel.count(),
+                postModel.find().skip(page*limit).limit(limit).populate('comments', '_id')
+             ])
+        const totalPages = Math.ceil(totalPosts/limit) - 1,
+              hasMore = page < totalPages;
+        //postModel.find().skip(page*limit).limit(limit).populate('comments', '_id');
+        //console.log(posts);
         return res.status(200).json({
-            count: postArray.length,
-            posts: postArray.map( postSingle => {
+            count: posts.length,
+            page,
+            totalPages,
+            hasMore,
+            posts: posts.map( postSingle => {
                 const {_id, title, cover, description, category, comments, date} = postSingle;
                 return {
                     _id,
@@ -37,18 +46,29 @@ router.get('/', async (req, res, next) => {
 
 //search post
 router.get('/search', async (req, res, next) => {
-    const keyword = req.query.keyword,
-          page = req.query.page || 0,
-          limit = req.query.limit || 2;
+    const keyword = req.query.keyword || '',
+          page = parseInt(req.query.page || 0),
+          limit = parseInt(req.query.limit || 2);
 
     const keyRex =  new RegExp(escapeSpace(keyword), 'gi');
-    console.log("sau khi escape: " + keyRex);
+    //console.log("sau khi escape: " + keyRex);
 
     try {
-        const posts = await postModel.find({title: keyRex}).skip(page*limit).limit(limit);
+        const  [totalPosts, posts] = await Promise.all(  
+            [   postModel.count({title: keyRex}),
+                postModel.find({title: keyRex})
+                         .skip(page*limit)
+                         .limit(limit)
+             ])
+        const totalPages = Math.ceil(totalPosts/limit) - 1,
+              hasMore = page < totalPages;
+
         if (posts !== null)
             return res.status(200).json({
                 count: posts.length,
+                page,
+                totalPages,
+                hasMore,
                 posts: posts.map(postSingle => {
                     return postSingle;
                 })
